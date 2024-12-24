cb4fe20f73d2424d7045de41e099a127b49afadf
SK <limsweekiat@gmail.com>
2024-12-24 17:47:17 +0800

    Support API v2

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -28,3 +28,30 @@ export async function streamAPI(
   const reader = response.body?.getReader()
   return reader as ReadableStreamDefaultReader<Uint8Array>
 }
+
+export async function streamAPIv2(
+  query = 'What am I covered for if I am warded in the hospital?',
+  history: ChatMessage[] = [],
+  current_policy: string | null = null,
+  document_ids: string[] = [],
+) {
+  const body = JSON.stringify({
+    query,
+    history,
+    current_policy,
+    document_ids,
+  })
+  const response = await fetch(API_URL + '/query', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body,
+  })
+  const responseJson = await response.json()
+  const convId = responseJson.id
+  const streamResponse = await fetch(API_URL + '/stream/' + convId)
+
+  const reader = streamResponse.body?.getReader()
+  return reader as ReadableStreamDefaultReader<Uint8Array>
+}
